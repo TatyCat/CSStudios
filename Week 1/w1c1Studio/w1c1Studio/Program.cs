7774caa497b49d469de7eac1c21637ad7531f40f
TatyCat <williams.tatyanna@gmail.com>
2018-02-07 16:06:14 -0500

    Bonus Studio 2 Complete

--- Week 1/w1c1Studio/w1c1Studio/Program.cs
+++ Week 1/w1c1Studio/w1c1Studio/Program.cs
@@ -6,30 +6,41 @@ namespace Area
     {
         static void Main(string[] args)
         {
-            //tell the user to enter a radius
-            Console.WriteLine("Provide a number for radius");
+            /*
+             1. User input a number
+             2. Check if the number is greater than / = 0
+             3. if it IS: continue with the program
+             4. If it is NOT: loop until the number is greater than 0
+             */
 
+            //User inputs number
+            Console.WriteLine("Provide a number for radius to calculate the area of your circle");
             string inputRad = Console.ReadLine();
 
             //convert the input into a number
             decimal radius = decimal.Parse(inputRad);
-
-            //Validation: Not a negative number
-            if (radius >= 0)
+            
+            //Re-prompt if number is negative
+            while(radius<0)
             {
-                //Calculates the area of a circle with the radius input
-                decimal pi = 3.14m;
-                decimal area = pi * radius * radius;
+                Console.WriteLine("Provide a positive number for radius");
+                string input = Console.ReadLine();
+                radius = decimal.Parse(input);
+            }
 
-                //Output
-                Console.WriteLine($"The area of your circle is {area}");
-                Console.ReadLine();
-            }
-            else
-            { 
-                Console.WriteLine("Input cannot be a negative number. Sorry, try again later.");
-                Console.ReadLine();
-            }
+            //Calculates the area of a circle with the radius input
+            decimal pi = 3.14m;
+            decimal area = pi * radius * radius;
+
+            //Output
+            Console.WriteLine($"The area of your circle is {area}");
+            Console.ReadLine();
         }
     }
 }
+
+
+/*
+ *Bonus Mission #2:
+ Extend your program further by using a while or do-while loop, so that when the user enters a negative number they are re-prompted. 
+ */
