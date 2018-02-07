8c608b2b003ef157097be7135c1077170d93b759
TatyCat <williams.tatyanna@gmail.com>
2018-02-07 02:57:19 -0500

    Area of a Circle Studio

---
+++ Week 1/w1c1Studio/w1c1Studio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Area
+{
+    class AreaOfCircle
+    {
+        static void Main(string[] args)
+        {
+            //tell the user to enter a radius
+            Console.WriteLine("Provide a number for radius");
+            string inputRad = Console.ReadLine();
+
+            //convert the input into a number
+            decimal radius = decimal.Parse(inputRad);
+
+            //Calculates the area of a circle with the radius input
+            decimal pi = 3.14m; 
+            decimal area = pi * radius * radius;
+
+            //Output
+            Console.WriteLine($"The area of your circle is {area}");
+     
+            Console.ReadLine();
+        }
+    }
+}
